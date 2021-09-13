bc56eabee2aa6bc64b5c4972aa840dcd9e17bb9a
gsrmlopes <gsrmlopes@gmail.com>
2021-09-13 19:47:51 -0300

    Update main.dart
    Adequação ao plano de fundo e inserção de opção de altMenu()

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,26 @@ import 'package:flutter/material.dart';
 
 import 'components/mainInfo.dart';
 import 'components/sideMenu.dart';
+//import 'components/altMenu.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Row(
-          children: [
-            Expanded(flex: 1, child: SideMenu()),
-            Expanded(flex: 5, child: MainInfo())
-          ],
+        body: Container(
+          color: Colors.grey.shade300,
+          child: Row(
+            children: [
+              Expanded(flex: 1, child: SideMenu()),
+              Expanded(flex: 5, child: MainInfo())
+            ],
+          ),
         ),
       ),
     );
